97f0e23e02b58bfb0e73ebd4f979e7ed514eee96
antoine carbonnel <antoinecarbonnelpro@yahoo.com>
2022-05-17 22:27:42 +0200

    first commit init config && setup fonts and scss && layout

---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="login">
+    <div class="container">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
